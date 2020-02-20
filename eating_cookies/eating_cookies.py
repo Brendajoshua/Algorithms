365118a7ac8e5b46412ea76efb238cb0924fd51b
Brenda Joshua <brendajosh.bj@gmail.com>
2020-02-20 17:47:24 +0300

    add eating cookies cache pseudocode

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,20 +7,29 @@ import sys
 # recursive solution
 def eating_cookies(n, cache=None):
    # cache solution
+   # if n is less than 0, return 0
     if n < 0:
       return 0
-
+    # if there is no cache
     if cache is None:
+      # create a cache using a range based loop
       cache = [0 for i in range(n + 1)]
     
+    #cache at index 0 is equal to 1
     cache[0] = 1
+    # if n is greater than 0
     if n > 0:
+      # the cache at index 1 is 1
       cache[1] = 1
+    # if n is gretaer than 1
     if n > 1:
+      # cache at index 2 is 2
       cache[2] = 2
-
+    # if n is equal to zero
     if cache[n] == 0:
+      # set the cache at n to the recursive call passing the cache
       cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    #return cache at n
     return cache[n]
 
     #non cache solution
